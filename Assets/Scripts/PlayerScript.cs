d31bb92553c2d6d26fd029c76e081b3ee76b5254
daniel velasco <knight@ucf.edu>
2021-03-09 20:28:40 -0500

    First commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,11 +10,13 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     public Text score;
     private int scoreValue = 0;
+    public Text winText;
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        winText.text= "";
     }
     
 
@@ -32,8 +34,18 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            SetScoreValue ();
         }
+    
 
+    }
+    void SetScoreValue()
+    {
+        score.text = "" + scoreValue.ToString ();
+       if (scoreValue >= 4) 
+       {
+           winText.text = "You Win! Game created by Daniel Velasco";
+       }
     }
     private void OnCollisionStay2D(Collision2D collision)
      {
@@ -41,7 +53,7 @@ public class PlayerScript : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0, 4), ForceMode2D.Impulse);
             }
             if (Input.GetKey("escape"))
             {
